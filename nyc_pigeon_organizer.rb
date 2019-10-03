dc44c15d7da112d47a5eade12d6bdb95087cc83d
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-10-03 03:35:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-  
+  list = names_all_pigeon(data)
+  pigeons={}
+  i = 0
+  while list[i] do
+     pigeons[list[i]] = {}
+     data.each_key do |up_key|
+               (pigeons[list[i]][up_key]=[]
+              data[up_key].each_key do |key|
+                    
+                    ((pigeons[list[i]][up_key]).push("#{key}")) if data[up_key][key].include?(list[i])
+                    
+     end)
+    end
+    i +=1
+  end
+  pigeons
 end
