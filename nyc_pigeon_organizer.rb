31c5329ef8a8a7669f3bc46bc194c520d721bfce
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-10-03 03:37:13 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+ def names_all_pigeon(data)
+ list_names = []
+    data.each_key do |up_key| data[up_key].each_key do |key| 
+                                s=(data[up_key][key]).size
+                                s.times { |i| list_names.push(data[up_key][key][i-1])}
+            end
+      end
+     list_names.uniq
+ end
+#names_all_pigeon(pigeon_data)
+
 def nyc_pigeon_organizer(data)
   list = names_all_pigeon(data)
   pigeons={}
